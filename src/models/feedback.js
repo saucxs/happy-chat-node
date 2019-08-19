14442e3ebbdbf64b14a363445aea03a7b2e14467
saucxs <saucxs@163.com>
2019-08-19 13:51:43 +0800

    fix:留言反馈存在的问题

--- src/models/feedback.js
+++ src/models/feedback.js
@@ -12,7 +12,6 @@ let getFeedbackList = function(pageIndex, pageNum) {
 // 提交留言反馈
 let submitFeedback = function(type, user_id, email, content, clientIP, device, loginDate) {
     const data = [type, user_id, email, content, clientIP, device, loginDate, '1'];
-    console.log(data, '-=-=')
     let _sql = `insert into feed_back(type, user_id, email, content, ip, device, date, is_show) values(?,?,?,?,?,?,?,?)`;
     return query(_sql, data)
 }
